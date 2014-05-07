7e5283039904c2ad518a61060017676902f59620
Nial Darbey <nialdarbey@gmail.com>
2014-05-06 23:24:30 -0300

    final

---
+++ src/test/resources/wishlist.groovy
@@ -0,0 +1,5 @@
+[ 'PageInfo' : [ 'pageIndex' : 10, 'pageSize' : 5 ], 'ItemSku' : 
+	[
+		[ 'id' : 'xxx', 'sku': 'xxx', 'name': 'Nial', 'summary' : 'Nice', 'price' : 12.30, 
+			'stock_quantity' : 7,
+'brand' : 'New House', 'type' : 'Perfumes', 'images' : [ ['url' : 'http://xxx', 'image_type_name' : 'SmallImage'], ['url' : 'http://zzz', 'image_type_name' : 'LargeImage'] ] ]] ]
